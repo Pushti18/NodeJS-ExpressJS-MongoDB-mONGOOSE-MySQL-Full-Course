2952bda87fbdeeb48c2fd5ab65fde1d3324cebb3
Pushti18 <pushti.depani110402@marwadiuniversity.ac.in>
2022-07-05 17:52:03 +0530

    warmup-1

--- 1-JAVASCRIPT/Assignments/Warmup-1/everyNth.js
+++ 1-JAVASCRIPT/Assignments/Warmup-1/everyNth.js
@@ -1,9 +1,8 @@
 function everyNth(str, n) {
   let p = "";
-  for (let i = 0; i < str.length; i = +n) {
+  for (let i = 0; i < str.length; i = i + n) {
     p = p + str.charAt(i);
   }
-
   return p;
 }
 let ans = everyNth("Code", 3);
