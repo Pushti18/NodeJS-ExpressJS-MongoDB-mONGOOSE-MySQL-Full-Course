51ed049866bd08c13082d633e448905b07f0b2fa
Pushti18 <pushti.depani110402@marwadiuniversity.ac.in>
2022-07-07 09:20:45 +0530

    string-2

--- 1-JAVASCRIPT/Assignments/String-2/doubleChar.js
+++ 1-JAVASCRIPT/Assignments/String-2/doubleChar.js
@@ -1,6 +1,6 @@
 function doubleChar(str) {
   let a = "";
-  for (var i = 0; i < str.length; i++) a = a + str.charAt(i) + str.charAt(i);
+  for (let i = 0; i < str.length; i++) a = a + str.charAt(i) + str.charAt(i);
   return a;
 }
 let ans = doubleChar("the");
